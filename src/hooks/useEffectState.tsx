9eff81a3a2ff16375009baba263c25ab1ab6a928
二货机器人 <smith3816@gmail.com>
2023-03-21 23:27:53 +0800

    refactor: merge effect at once

--- src/hooks/useEffectState.tsx
+++ src/hooks/useEffectState.tsx
@@ -1,44 +1,41 @@
-import useLayoutEffect from 'rc-util/lib/hooks/useLayoutEffect';
 import useEvent from 'rc-util/lib/hooks/useEvent';
 import * as React from 'react';
+import { unstable_batchedUpdates } from 'react-dom';
 
 type Updater<T> = T | ((origin: T) => T);
 
-const EMPTY = {};
+type UpdateCallbackFunc = VoidFunction;
 
-type UpdateCallbackFunc = (first: boolean) => void;
-
-type NotifyEffectUpdate = (key: number, callback: UpdateCallbackFunc) => void;
+type NotifyEffectUpdate = (callback: UpdateCallbackFunc) => void;
 
 /**
  * Batcher for record any `useEffectState` need update.
  */
 export function useBatcher() {
   // Updater Trigger
-  const [updateCount, setUpdateCount] = React.useState(0);
-  const updateFuncRef =
-    React.useRef<UpdateCallbackFunc[]>(null);
-
-  // Effect updater
-  useLayoutEffect(() => {
-    if (updateCount !== 0) {
-      const keys: Record<number, boolean> = {};
-      updateFuncRef.current.forEach(([key, callback]) => {
-        callback(!keys[key]);
-        keys[key] = true;
-      });
-      updateFuncRef.current = null;
-    }
-  }, [updateCount]);
+  const updateFuncRef = React.useRef<UpdateCallbackFunc[]>(null);
 
   // Notify update
-  const notifyEffectUpdate: NotifyEffectUpdate = (key, callback) => {
+  const notifyEffectUpdate: NotifyEffectUpdate = callback => {
+    // console.error('Update');
     if (!updateFuncRef.current) {
       updateFuncRef.current = [];
-      setUpdateCount(c => c + 1);
+
+      const channel = new MessageChannel();
+      channel.port1.onmessage = () => {
+        // console.log('!!! channel update !!!');
+        unstable_batchedUpdates(() => {
+          updateFuncRef.current.forEach(fn => {
+            fn();
+          });
+          updateFuncRef.current = null;
+        });
+      };
+      channel.port2.postMessage(undefined);
     }
 
-    updateFuncRef.current.push([key, callback]);
+    // console.log('push');
+    updateFuncRef.current.push(callback);
   };
 
   return notifyEffectUpdate;
@@ -53,38 +50,12 @@ export default function useEffectState<T extends string | number | object>(
 ): [T, (value: Updater<T>) => void] {
   // Value
   const [stateValue, setStateValue] = React.useState(defaultValue);
-  const valRef = React.useRef<T | typeof EMPTY>(EMPTY);
-
-  // Updater Trigger
-  const [updateCount, setUpdateCount] = React.useState(0);
-
-  // Effect Updater
-  useLayoutEffect(() => {
-    if (updateCount !== 0) {
-      setStateValue(valRef.current as T);
-      valRef.current = EMPTY;
-    }
-  }, [updateCount]);
 
   // Set State
   const setEffectVal = useEvent((nextValue: Updater<T>) => {
-    let mergedNextValue: T;
-
-    const init = valRef.current === EMPTY;
-
-    if (typeof nextValue === 'function') {
-      if (init) {
-        valRef.current = stateValue;
-      }
-      mergedNextValue = nextValue(valRef.current as T);
-    } else {
-      mergedNextValue = nextValue;
-    }
-    valRef.current = mergedNextValue;
-
-    if (init) {
-      setUpdateCount(c => c + 1);
-    }
+    notifyEffectUpdate(() => {
+      setStateValue(nextValue);
+    });
   });
 
   return [stateValue, setEffectVal];

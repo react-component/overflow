dc4a559d991b705cf8b72cd2c7ee176a935a38e7
liangjun.chen <liangjun.chen@yunqutech.com>
2023-12-06 10:28:21 +0800

    chore: vercel deploy

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'dumi';
+const isGitPagesSite = process.env.GITHUB_ACTIONS;
 
 export default defineConfig({
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
@@ -8,8 +9,8 @@ export default defineConfig({
   },
   exportStatic: {},
   outputPath: 'docs-dist',
-  base: '/rc-overflow/',
-  publicPath: '/rc-overflow/',
+  base: isGitPagesSite ? `/rc-overflow/` : ``,
+  publicPath: isGitPagesSite ? `/rc-overflow/` : ``,
   styles: [
     `
       .markdown table {

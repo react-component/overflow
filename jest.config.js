9faf88e02cbf03b80981b47994b7af50eda5db20
刘欢 <lh01217311@antgroup.com>
2025-01-08 15:12:48 +0800

    chore:更新jest配置

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   setupFiles: ['./tests/setup.js'],
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
+  transformIgnorePatterns: [
+    '/node_modules/(?!cheerio).+\\.js$',
+  ],
 };
